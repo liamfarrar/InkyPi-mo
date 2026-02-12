a26b320d445abeb7d690e32e961469dc21bcd386
liamfarrar <liam.farrar@live.co.uk>
2026-02-12 22:31:11 +0000

    update

--- src/display/inky_display.py
+++ src/display/inky_display.py
@@ -27,7 +27,7 @@ class InkyDisplay(AbstractDisplay):
             ValueError: If the resolution cannot be retrieved or stored.
         """
         
-        self.inky_display = auto()
+        self.inky_display = auto(800,480)
         self.inky_display.set_border(self.inky_display.BLACK)
 
         # store display resolution in device config
